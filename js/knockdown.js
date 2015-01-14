9ead3ee0a5fb20dfb2c045a789a6d2b0bdd3e224
qdonnellan <quentin@burkesoftware.com>
2015-01-14 08:33:24 -0600

    removed mathjax added katex

--- js/knockdown.js
+++ js/knockdown.js
@@ -1,37 +1,39 @@
 function ViewModel() {
     var self = this;
-    self.sandboxInput = ko.observable('')
+    self.sandboxInput = ko.observable('');
     self.sandboxOutput = ko.computed(function() {
         var input_str = self.sandboxInput();
-        var links = input_str.match(/\bhttps?[:][\/][\/]\S*\b/g);
-        if (links) {
-            for (var i = 0; i < links.length; i++) {
-                var link = links[i];
-                input_str = input_str.replace(link, self.formatLink(link));
+        var inlineEquations = input_str.match(/\$.*\$/g);
+        var e, html, katexHtml, equationPlaceholder, equation;
+        if (inlineEquations) {
+            for (e in inlineEquations) {
+                equationPlaceholder = "::EQUATION-" + e + "::";
+                equation = inlineEquations[e];
+                input_str = input_str.replace(equation, equationPlaceholder);
             }
         }
-        //remove the link placeholder
-        input_str = input_str.replace(new RegExp('::LINK::PLACEHOLDER::', 'g'), 'http');
-        return markdown.toHTML(input_str);
+        html = markdown.toHTML(input_str);
+
+        if (inlineEquations) {
+            for (e in inlineEquations) {
+                equationPlaceholder = "::EQUATION-" + e + "::";
+                equation = inlineEquations[e];
+                console.log(equation);
+                equation = self.stripDollarSignsEquation(equation);
+                console.log(equation);
+                katexHtml = katex.renderToString(equation);
+                html = html.replace(equationPlaceholder, katexHtml);
+            }
+        }
+
+        return html;
     });
 
-    self.formatLink = function(link) {
-        //add a link placeholder so that we don't replace the same link more than once
-        link = link.replace('http', '::LINK::PLACEHOLDER::');
-        if (link.match(/\S*[.]png|jpg|bmp|jpeg|tiff|tif|gif|jp2\b/)) {
-            return "![" + link + "](" + link + ")";
-        }
-        else {
-            return "[" + link + "](" + link + ")";
-        }  
+    self.stripDollarSignsEquation = function(eqn) {
+        return eqn.replace(/\$/g, "");
     };
 };
 
-// make sure mathjax is called after each new character input
-// in the text area; displays math real time
-$("#sandbox-textarea").keyup(function () {
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-});
 
 vm = new ViewModel();
 ko.applyBindings(vm);
